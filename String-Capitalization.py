5a24ea7247e94128dcd5fd63ed2e3db266680ccf
Infance Tony.E <138364215+InfanceTony-cs22@users.noreply.github.com>
2023-10-12 20:05:22 +0530

    Create String-Capitalization.py

---
+++ String-Capitalization.py
@@ -0,0 +1,9 @@
+def solve(s):
+    # Capitalize the first letter of each word in the string
+    capitalized_string = s.title()
+    return capitalized_string
+
+# Example usage:
+input_string = "infance tony"
+result = solve(input_string)
+print(result)  # This will print "Chris Alan"
